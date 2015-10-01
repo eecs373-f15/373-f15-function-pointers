31ab65a624218bfa31d295dfa1df31366ca1d23f
xuzhuyun <xuzhuyun@umich.edu>
2015-09-30 20:07:23 -0400

    Update sort.c

--- sort.c
+++ sort.c
@@ -57,5 +57,14 @@ void insertion_sort(int *numbers, unsigned count) {
 	memcpy(numbers, new, count*sizeof(int));
 }
 
-sorting_fn sorting_fns[] = {bubble_sort, insertion_sort, NULL};
+
+static int q_compare(const void *a, const void *b) {
+	    return (*(int *)a - *(int *)b);
+}
+
+void q_sort(int *numbers, unsigned count){
+	qsort (&numbers[0], count, 4, q_compare);
+}
+
+sorting_fn sorting_fns[] = {bubble_sort, insertion_sort, q_sort, NULL};
 
